275c339aa0fa80a693e09a55b481f41c7d2c061e
prem kumar <151729765+prem1kr@users.noreply.github.com>
2025-04-18 20:38:00 +0530

    Update Login.jsx

--- frontend/src/pages/Authontiction/Login.jsx
+++ frontend/src/pages/Authontiction/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     setError(null);
 
     try {
-      const response = await fetch("https://bluestock-fintech-fx2w.onrender.com/api/auth/login", {
+      const response = await fetch("https://bluestock-fintech-bw90.onrender.com/api/auth/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
